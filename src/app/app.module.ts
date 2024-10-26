7d4c2e6e58f18dc218ecc2b5c56dac72543a3fd0
Mahi <heroda02@gmail.com>
2024-10-26 20:10:43 +0530

    ADDED ALL FILES

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { AboutComponent } from './views/about/about.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { AssessmentsComponent } from './views/assessments/assessments.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    AssessmentsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
